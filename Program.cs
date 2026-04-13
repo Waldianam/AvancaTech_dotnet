0909a0d990d0db8f564741e642c0b0757ae0c191
Waldianam <waldianam@gmail.com>
2026-04-13 11:49:51 -0300

    Aula1 C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+string nome = "Beatriz";
+int idade= 20;
+float altura = 1.70f;
+Boolean temcarteira = true; 
+
+
+
+
+
+Console.WriteLine("Olá, meu nome é" + nome + "tenho"  + idade + "anos e" + altura+ "de altura");
+Console.WriteLine(temcarteira);
+ 
+
+
+
