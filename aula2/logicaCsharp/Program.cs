70c77f43d95561325540495df8cb92ae75e67403
Waldianam <waldianam@gmail.com>
2026-04-15 11:26:35 -0300

    aula2 c#

---
+++ aula2/logicaCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿// Criando Variáveis
+string nome = "Waldiana";
+int idade = 20;
+
+Console.WriteLine(nome);
+Console.WriteLine(idade);
+
+//condicionais
+
+if (idade >=18)
+{
+    Console.WriteLine(nome +" é maior de idade");
+
+}
+else
+{
+    Console.WriteLine( nome + " é menor de idade");
+}
+
+//Loop
+for( int i = 0; i<= 10; i++)
+{
+    Console.WriteLine("Numero:" + i);
+}
